2553322b0266d33fc093414056742f5eb7cb019b
Osmandi <osmandigt@gmail.com>
2025-01-13 11:08:22 -0500

    Adding terraform code

---
+++ Fase 1 - Creación de la infraestructura para análisis Big Data/main.tf
@@ -0,0 +1,60 @@
+provider "google" {
+  project = var.project_id
+  region  = var.region
+}
+
+# En este bucket se guardan los notebooks
+#resource "google_storage_bucket" "tf_gcs_bucket" {
+#  name     = var.bucket_name
+#  location = "US"
+#  force_destroy = true
+#}
+
+resource "google_dataproc_cluster" "bigdata_dataproc2" {
+  name   = var.dataproc_name
+  region = var.region
+
+  cluster_config {
+
+    staging_bucket = var.bucket_name
+
+    # Accessing to the app
+    ## Configuración por http: enable_http_port_access = true; internal_ip_only=false
+    endpoint_config {
+      enable_http_port_access = false
+    }
+    gce_cluster_config {
+      internal_ip_only = false
+    }
+
+    master_config {
+      num_instances    = 1
+      machine_type     = var.instance_type_master
+      disk_config {
+        boot_disk_size_gb =var.boot_size_disk_master
+      }
+    }
+
+    worker_config {
+      num_instances    = var.worker_instances_number
+      machine_type     = var.instance_type_worker
+      disk_config {
+        boot_disk_size_gb = var.boot_size_disk_worker
+      }
+    }
+
+    software_config {
+      image_version = "2.2-ubuntu22"
+      optional_components = [
+        "JUPYTER",
+        "ZOOKEEPER"
+      ]
+    }
+
+
+    # Delete cluster after 4 hours
+    #lifecycle_config {
+    #  idle_delete_ttl = "14400s" # 4 hours
+    #}
+  }
+}
---
+++ Fase 1 - Creación de la infraestructura para análisis Big Data/variables.tf
@@ -0,0 +1,47 @@
+variable "project_id" {
+  type = string
+  description = "ID of the project in which to create resources and add IAM bindings."
+}
+
+variable "region" {
+  type = string
+  description = "The Amazon Web Service region to deploy to"
+}
+
+variable bucket_name {
+  type = string
+  description = "Name of the Google Cloud Storage bucket that will contain the objects."
+}
+
+variable dataproc_name {
+  type = string
+  description = "Name of Dataproc cluster"
+}
+
+variable worker_instances_number {
+  type = number
+  description = "Number of instances to start"
+  default = 2
+}
+
+variable boot_size_disk_master {
+  type = number
+  description = "Size of disc in master node"
+  default = 100
+}
+
+variable boot_size_disk_worker {
+  type = number
+  description = "Size of disc in worker node"
+  default = 100
+}
+
+variable "instance_type_master" {
+  type = string
+  description = "The type of VM instance for master"
+}
+
+variable "instance_type_worker" {
+  type = string
+  description = "The type of VM instance for workers"
+}
