b8c39ffc35ad376fd8dd5f23dee7065fc6257ab8
Osmandi <osmandigt@gmail.com>
2025-01-14 13:26:35 -0500

    Adding EDA updates

--- Fase 3 - Realización del EDA/1-create_silver_layer.py
+++ Fase 3 - Realización del EDA/1-create_silver_layer.py
@@ -15,6 +15,10 @@ if __name__ == "__main__":
         # Read dataframe in silver
         df = spark.read.parquet("/TFM/data/bronze/velocidades_bitcarrier.parquet")
 
+        # Create ROUTE_ID column to identify unique combination of NAME_FROM and NAME_TO columns
+        df_route_id = df.select(F.col("NAME_FROM"), F.col("NAME_TO")).distinct().withColumn("ROUTE_ID", F.monotonically_increasing_id())
+        df = df.join(F.broadcast(df_route_id), ["NAME_FROM", "NAME_TO"], how="inner")
+
         # Filter DataFrame
         (
             df.withColumns(
@@ -27,38 +31,50 @@ if __name__ == "__main__":
                         "NAME_TO_2": F.replace(F.split_part(F.col("NAME_TO"), F.lit(";"), F.lit(2)), F.lit("CL"), F.lit("")).cast(T.IntegerType()),
                         "DIRECTION": F.when(F.col("NAME_TO_1") < F.col("NAME_TO_2"), F.lit("SOUTH-NORTH")).otherwise(F.lit("NORTH-SOUTH")),
                         "DIA_SEMANA": F.date_format(F.col("INICIO"), "EEEE"),
+                        "NUMERO_SEMANA": F.dayofweek(F.col("INICIO")),
                         "MES": F.date_format(F.col("INICIO"), "LLLL"),
+                        "MONTH_NUMBER": F.month(F.col("INICIO"))
                     }
             ).select(
                 F.col("INICIO"),
                 F.col("YEAR"),
                 F.col("HORA"),
                 F.col("MINUTE"),
+                F.col("CUARTO_HORA"),
                 F.col("DISTANCE"),
                 F.col("VEL_PROMEDIO"),
+                F.col("VEL_PONDERADA"),
                 F.col("DIA_SEMANA"),
+                F.col("NUMERO_SEMANA"),
                 F.col("MES"),
+                F.col("MONTH_NUMBER"),
                 F.col("NAME_FROM"),
                 F.col("NAME_TO"),
                 F.col("NAME_TO_1"),
                 F.col("NAME_TO_2"),
+                F.col("ROUTE_ID"),
                 F.col("DIRECTION"),
                 F.col("NUMDISPOSITIVOS")
             ).withColumnsRenamed({
-                "INICIO": "start",
+                "INICIO": "start_timestamp",
                 "YEAR": "year",
                 "HORA": "hour",
                 "MINUTE": "minute",
+                "CUARTO_HORA": "time",
                 "DISTANCE": "distance",
-                "VEL_PROMEDIO": "vel_avg",
+                "VEL_PROMEDIO": "speed_average",
+                "VEL_PONDERADA": "speed_weighted",
                 "DIA_SEMANA": "day_week_name",
+                "NUMERO_SEMANA": "day_week_number",
                 "MES": "month_name",
+                "MONTH_NUMBER": "month_number",
                 "NAME_FROM": "name_from",
                 "NAME_TO": "name_to",
                 "NAME_TO_1": "name_to_1",
                 "NAME_TO_2": "name_to_2",
+                "ROUTE_ID": "route_id",
                 "DIRECTION": "direction",
-                "NUMDISPOSITIVOS": "numdispositivos"
+                "NUMDISPOSITIVOS": "num_devices"
             })
         ).write.mode("overwrite").parquet("/TFM/data/silver/velocidades_bitcarrier.parquet")
 
